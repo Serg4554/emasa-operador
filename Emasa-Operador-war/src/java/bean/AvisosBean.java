118e39c6e9bb59545eb3752ec8b85ae9a2607084
Serg <Sergio.357.95@gmail.com>
2016-12-06 21:05:58 +0100

    Restructuración de código y modificar avisos

--- Emasa-Operador-war/src/java/bean/AvisosBean.java
+++ Emasa-Operador-war/src/java/bean/AvisosBean.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.ws.WebServiceRef;
@@ -27,7 +27,7 @@ import javax.xml.ws.WebServiceRef;
  * @author Serg
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class AvisosBean {
 
     @WebServiceRef(wsdlLocation = "http://localhost:8080/Emasa-Soap-war/AvisoWS?WSDL")
@@ -36,16 +36,12 @@ public class AvisosBean {
     @ManagedProperty(value="#{usuarioBean}")
     private UsuarioBean usuarioBean;
     
-    private String ubicacion;
-    private String estado;
-    private String observaciones;
-    private String ubicacionTecnica;
+    private Aviso avisoSeleccionado;
     private String prioridad;
     private String inicioReparacion;
     private String finReparacion;
     private String latitudGPS;
     private String longitudGPS;
-    private String tipo;
     private String error;
     
     /**
@@ -63,36 +59,12 @@ public class AvisosBean {
         return findAll();
     }
 
-    public String getUbicacion() {
-        return ubicacion;
+    public Aviso getAvisoSeleccionado() {
+        return avisoSeleccionado;
     }
 
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getObservaciones() {
-        return observaciones;
-    }
-
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
-    }
-
-    public String getUbicacionTecnica() {
-        return ubicacionTecnica;
-    }
-
-    public void setUbicacionTecnica(String ubicaciónTecnica) {
-        this.ubicacionTecnica = ubicaciónTecnica;
+    public void setAvisoSeleccionado(Aviso avisoSeleccionado) {
+        this.avisoSeleccionado = avisoSeleccionado;
     }
 
     public String getPrioridad() {
@@ -135,14 +107,6 @@ public class AvisosBean {
         this.longitudGPS = longitudGPS;
     }
 
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     public String getError() {
         return error;
     }
@@ -152,43 +116,75 @@ public class AvisosBean {
     }
     
     public String doCrear() {
-        return "nuevoAviso";
+        setAvisoSeleccionado(new Aviso());
+        error = "";
+        prioridad = null;
+        inicioReparacion = null;
+        finReparacion = null;
+        latitudGPS = null;
+        longitudGPS = null;
+        return "editarAviso";
+    }
+    
+    public String doEditar(Aviso aviso) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+        formatter.applyPattern("dd-MM-yyyy");
+        
+        setAvisoSeleccionado(aviso);
+        error = "";
+        
+        if(avisoSeleccionado.getPrioridad() != null) {
+            prioridad = ""+avisoSeleccionado.getPrioridad();
+        } else {
+            prioridad = null;
+        }
+        
+        if(avisoSeleccionado.getInicioReparacion() != null) {
+            inicioReparacion = formatter.format(avisoSeleccionado.getInicioReparacion().toGregorianCalendar().getTime());
+        } else {
+            inicioReparacion = null;
+        }
+        
+        if(avisoSeleccionado.getFinReparacion() != null) {
+            finReparacion = formatter.format(avisoSeleccionado.getFinReparacion().toGregorianCalendar().getTime());
+        } else {
+            finReparacion = null;
+        }
+        
+        if(avisoSeleccionado.getPosGPS() != null) {
+            String[] posGPS = avisoSeleccionado.getPosGPS().split(";");
+            latitudGPS = posGPS[0];
+            longitudGPS = posGPS[1];
+        } else {
+            latitudGPS = null;
+            longitudGPS = null;
+        }
+        
+        return "editarAviso";
     }
     
     public String doGuardar() {
-        Aviso aviso = new Aviso();
-        
         try {
             GregorianCalendar c = new GregorianCalendar();
             c.setTime(new Date());
-            aviso.setFechacreacion(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
+            avisoSeleccionado.setFechacreacion(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
         } catch (DatatypeConfigurationException ex) {
             Logger.getLogger(AvisosBean.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        if (ubicacion == null || ubicacion.trim().isEmpty()) {
+        if (avisoSeleccionado.getUbicacion() == null || avisoSeleccionado.getUbicacion().trim().isEmpty()) {
             error = "Ubicación inválida";
-            return "nuevoAviso";
-        } else {
-            aviso.setUbicacion(ubicacion);
+            return "editarAviso";
         }
         
-        if(estado == null || estado.trim().isEmpty()) {
+        if(avisoSeleccionado.getEstado() == null || avisoSeleccionado.getEstado().trim().isEmpty()) {
             error = "El campo estado no puede estar vacío";
-            return "nuevoAviso";
-        } else {
-            aviso.setEstado(estado);
+            return "editarAviso";
         }
         
-        if (observaciones == null || observaciones.trim().isEmpty()) {
+        if (avisoSeleccionado.getObservaciones() == null || avisoSeleccionado.getObservaciones().trim().isEmpty()) {
             error = "El campo observaciones no puede estar vacío";
-            return "nuevoAviso";
-        } else {
-            aviso.setObservaciones(observaciones);
-        }
-        
-        if(ubicacionTecnica != null && !ubicacionTecnica.trim().isEmpty()) {
-            aviso.setUbicacionTecnica(ubicacionTecnica);
+            return "editarAviso";
         }
         
         if(prioridad != null && !prioridad.trim().isEmpty()) {
@@ -196,12 +192,12 @@ public class AvisosBean {
                 int num = Integer.parseInt(prioridad);
                 if(num < 1 || num > 9) {
                     error = "La prioridad debe estar entre 1 y 9";
-                    return "nuevoAviso";
+                    return "editarAviso";
                 }
-                aviso.setPrioridad(num);
+                avisoSeleccionado.setPrioridad(num);
             } catch(NumberFormatException e) {
                 error = "La prioridad debe ser un número";
-                return "nuevoAviso";
+                return "editarAviso";
             }
         }
         
@@ -212,10 +208,10 @@ public class AvisosBean {
                 Date date = formatter.parse(inicioReparacion);
                 GregorianCalendar c = new GregorianCalendar();
                 c.setTime(date);
-                aviso.setInicioReparacion(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
+                avisoSeleccionado.setInicioReparacion(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
             } catch (DatatypeConfigurationException | ParseException e) {
                 error = "El formato de fecha debe ser dd-MM-yyyy";
-                return "nuevoAviso";
+                return "editarAviso";
             }
         }
         
@@ -226,20 +222,21 @@ public class AvisosBean {
                 Date date = formatter.parse(finReparacion);
                 GregorianCalendar c = new GregorianCalendar();
                 c.setTime(date);
-                aviso.setFinReparacion(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
+                avisoSeleccionado.setFinReparacion(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
             } catch (DatatypeConfigurationException | ParseException e) {
                 error = "El formato de fecha debe ser dd-MM-yyyy";
-                return "nuevoAviso";
+                return "editarAviso";
             }
         }
         
-        if((latitudGPS != null && !latitudGPS.trim().isEmpty()) &&
-                (longitudGPS == null || longitudGPS.trim().isEmpty())) {
-            error = "La longitud no puede estar vacía";
-            return "nuevoAviso";
+        if(latitudGPS != null && !latitudGPS.trim().isEmpty()) {
+            if(longitudGPS == null || longitudGPS.trim().isEmpty()) {
+                error = "La longitud no puede estar vacía";
+                return "editarAviso";
+             }
         } else if(longitudGPS != null && !longitudGPS.trim().isEmpty()) {
             error = "La latitud no puede estar vacía";
-            return "nuevoAviso";
+            return "editarAviso";
         }
         
         if((latitudGPS != null && !latitudGPS.trim().isEmpty()) &&
@@ -247,24 +244,24 @@ public class AvisosBean {
             try {
                 Double.parseDouble(longitudGPS);
                 Double.parseDouble(latitudGPS);
-                aviso.setPosGPS(latitudGPS+";"+longitudGPS);
+                avisoSeleccionado.setPosGPS(latitudGPS+";"+longitudGPS);
             } catch(NumberFormatException e) {
                 error = "La latitud y longitud deben ser numéricos";
-                return "nuevoAviso";
+                return "editarAviso";
             }
-        }
-        
-        if (tipo != null && !tipo.trim().isEmpty()) {
-            aviso.setTipo(tipo);
         }
         
         avisows.Usuario _usuario = new avisows.Usuario();
         _usuario.setEmail(usuarioBean.getUsuario().getEmail());
         _usuario.setOperador(usuarioBean.getUsuario().isOperador());
         
-        aviso.setUsuarioemail(_usuario);
+        avisoSeleccionado.setUsuarioemail(_usuario);
         
-        create(aviso);
+        if(avisoSeleccionado.getId() == null) {
+            create(avisoSeleccionado);
+        } else {
+            edit(avisoSeleccionado);
+        }
         
         return "listaAvisos?faces-redirect=true";
     }
@@ -293,5 +290,12 @@ public class AvisosBean {
         // If the calling of port operations may lead to race condition some synchronization is required.
         avisows.AvisoWS port = service.getAvisoWSPort();
         port.remove(entity);
+    }
+
+    private void edit(avisows.Aviso entity) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        avisows.AvisoWS port = service.getAvisoWSPort();
+        port.edit(entity);
     }
 }
